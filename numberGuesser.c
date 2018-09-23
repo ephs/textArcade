13bb3b3bc13c8d0dd18840df87dcd7c17627257c
Jythonscript <90302927@ep-student.org>
2018-09-22 23:00:24 -0500

    remove debugging leftovers

--- numberGuesser.c
+++ numberGuesser.c
@@ -11,8 +11,6 @@ static int STARTINGLIVES = 6;
 
 void numberGuesser() {
 
-	printf("%d\n", atoi("asdfasdfa"));
-
 	srand(time(NULL));
 	int randNum, lives, numGuess;
 	char guess[MAXGUESSLEN], c;
@@ -55,7 +53,7 @@ start:
 	else {
 		printf("You Lose! The correct number was %d. Would you like to try again? (y/n)\n", randNum);
 		while ((c = getchar()) != 'y' && c != 'n')
-			//printf("(y/n)\n");
+			;
 		if (c == 'y') {
 			goto start;
 		}
@@ -69,7 +67,6 @@ int playerNumGuess() {
 	char guess[MAXGUESSLEN];
 	int intGuess;
 	do {
-		//printf("in playerNumGuess do-while");
 		scanf("%s", guess);
 		intGuess = atoi(guess);
 
